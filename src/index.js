91852eb2254948498f2ad95b460d507d20265a58
Andrey Diomov <andrey.diomov@gmail.com>
2023-08-26 15:38:39 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const singleDigits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teenDigits = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tensDigits = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 10) {
+        return singleDigits[number];
+    } else if (number > 10 && number < 20) {
+        return teenDigits[number % 10];
+    } else if (number < 100 && number % 10 == 0) {
+        return tensDigits[number / 10];
+    } else if (number > 20 && number < 100) {
+        return tensDigits[Math.floor(number / 10)] + ' ' + singleDigits[number % 10];
+    } else if (number % 100 == 0) {
+        return singleDigits[number / 100] + ' hundred'
+    } else if (number > 100 && number < 1000) {
+        let hundred = Math.floor(number / 100);
+        let tens = Math.floor((number % 100) / 10);
+        let digit = number % 10;
+
+        if (tens == 1 && digit != 0) { return singleDigits[hundred] + ' hundred ' + teenDigits[digit]; }
+        if (tens == 0 && digit != 0) { return singleDigits[hundred] + ' hundred ' + singleDigits[digit]; }
+        if (digit == 0) { return singleDigits[hundred] + ' hundred ' + tensDigits[tens]; }
+
+        return singleDigits[hundred] + ' hundred ' + tensDigits[tens] + ' ' + singleDigits[digit];
+    }
 }
